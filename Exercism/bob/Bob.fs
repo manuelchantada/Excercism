40ef288fa6be10461575b052587ce5b0d2339280
Manuel Chantada <mchantada@gpm.local>
2019-08-13 15:44:16 +0200

    Initial commit

---
+++ Exercism/bob/Bob.fs
@@ -0,0 +1,24 @@
+﻿module Bob
+
+let response (input: string): string =  
+    let isQuestion (s: string) = 
+        s.TrimEnd().EndsWith("?")
+
+    let isShouting (s: string) =
+        s.ToUpper() = s && s.ToLower() <> s
+    
+    let isExclaming (s: string) =      
+        s.EndsWith("!")
+
+    let isSilence (s: string) =
+        s.Trim() = ""
+
+    match input with
+    |_ when isQuestion(input) & isShouting(input) -> "Calm down, I know what I'm doing!"
+    |_ when isQuestion(input) -> "Sure."
+    |_ when isShouting(input) -> "Whoa, chill out!"
+    |_ when isExclaming(input) -> "Whatever."
+    |_ when isSilence(input) -> "Fine. Be that way!"
+    |_ -> "Whatever."
+    
+
